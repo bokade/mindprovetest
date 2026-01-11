c8a176bda4807f82fc5c70b20286dd4348eda7dc
Swapnil Bokade <sbokade181@gmail.com>
2026-01-11 15:03:29 +0530

    test

--- src/main/java/com/example/MindProveTest/config/SecurityConfig.java
+++ src/main/java/com/example/MindProveTest/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -46,13 +47,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**").hasAnyRole("ADMIN").requestMatchers("/user/**","/admin/**").hasAnyRole("ADMIN", "USER") // login page sab ke liye open
 
-                );
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());   // 🔥 VERY IMPORTANT
+
         return http.build();
     }
+
 
 
 
